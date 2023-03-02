cdbde45caa8c1d860e00086ca5aa746e5cb1ebf7
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-02 20:28:11 +0000

    Add TiposVarios

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            uint b = 2U;
+            long c = 0x03L;
+            ulong d = 0b100UL;
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+        }
+    }
+}
