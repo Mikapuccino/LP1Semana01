1d99f7ab559ef759eea895670ef935a1a662e57d
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-02 20:12:53 +0000

    Add comment to OlaMundo

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -4,6 +4,10 @@ namespace OlaMundo
 {
     class Program
     {
+        /// <summary>
+        /// Writes "Hello World" and "Bye World" in the console
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
