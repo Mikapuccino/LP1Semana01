18067c5a42c8f4931f24d523c160049d0cb63ff7
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-02 20:08:52 +0000

    Add Argumentos

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
-namespace OlaMundo
+namespace Argumentos
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Bye World!");
+            for (int i = 0; i < args.Length; i++)
+            Console.WriteLine(args[i]);
         }
     }
 }
