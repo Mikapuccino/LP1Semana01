7b4997c2fa637a4a17c65afbc1561c133c265f9b
Nelson <nelsonfcmilheiro@gmail.com>
2023-03-03 14:30:58 +0000

    Fix indentation

--- ArgLongStrings/Program.cs
+++ ArgLongStrings/Program.cs
@@ -7,8 +7,13 @@ namespace ArgLongStrings
         static void Main(string[] args)
         {
             for (int i = 0; i < args.Length; i++)
+            {
                 if (args[i].Length > 4)
+                {
                     Console.WriteLine(args[i]);
+                }
+            }
+                
         }
     }
 }
